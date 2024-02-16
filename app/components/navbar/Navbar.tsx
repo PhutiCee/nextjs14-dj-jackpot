0f2fe43b1dfbe5a0f18b5fb85818a6942f5822ee
PhutiCee <72173500+PhutiCee@users.noreply.github.com>
2024-02-16 23:58:42 +0200

    initial commit

---
+++ app/components/navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <div>
+            <nav>
+                <h2>Dj Jackpot</h2>
+                <ul>
+                    <li>Events</li>
+                    <li>About</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
